447f590602b7bbfc3bb06c6779d126cc8819f2c4
Duy Dinh <ducduy9304@gmail.com>
2025-05-27 11:15:42 +0700

    Update project_to_bev.py

--- project_to_bev.py
+++ project_to_bev.py
@@ -5,5 +5,5 @@ def to_bev(u, v, H, x_origin, y_origin, scale):
     ground_point = np.linalg.inv(H) @ point
     ground_point /= ground_point[2]
     x_map = int((ground_point[0] + x_origin) * scale)
-    y_map = int((33.5 - ground_point[1]) * scale)
+    y_map = int((33.5 - ground_point[1]) * scale) # dùng 33.5 nếu muốn show đúng trên bev map, còn dùng y_origin trong calib thì sẽ bị lệch
     return x_map, y_map
